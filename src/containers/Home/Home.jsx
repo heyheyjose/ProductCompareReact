ed12d5cb9312fecbfbd3ba12d395b2cedd40fe45
Jose Vazquez <josevazquez7@deloitte.com>
2021-03-15 15:54:57 -0400

    convert to hooks

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,61 +1,54 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { CompareTable, ProductList } from "../../components";
 import results from "../../data/products.json";
 
-class Home extends Component {
-  constructor() {
-    super();
-    this.state = {
-      nameSearch: "",
-      products: results.products,
-      priceSearch: undefined
-    };
-  }
+const Home = () => {
+  const [nameSearch, setNameSearch] = useState('');
+  const [products, setProducts] = useState(results.products);
+  const [priceSearch, setPriceSearch] = useState(undefined);
 
-  render() {
-    const compareProducts = this.state.products.filter(
-      (product) => product.compare
+  const compareProducts = products.filter(
+    (product) => product.compare
+  );
+
+  const handleChange = (event) => {
+    setPriceSearch(event.target.value);
+  };
+
+  const compare = (id) => {
+    setProducts(
+     products.map((product) =>
+        product.id === id
+          ? { ...product, compare: !product.compare }
+          : product
+      ),
     );
-    const handleChange = (event) => {
-      console.log('event: ', event);
-      this.setState({ priceSearch: event.target.value });
-    };
+  };
 
-    const compare = (id) => {
-      this.setState({
-        products: this.state.products.map((product) =>
-          product.id === id
-            ? { ...product, compare: !product.compare }
-            : product
-        ),
-      });
-    };
-
-    return (
-      <div className="home mt-5">
-        <div className="row">
-          <div className="col-12">
-            <h2 className="mb-3">Compare Products</h2>
-            <input
-              type="number"
-              placeholder="Search by price"
-              value={this.state.priceSearch}
-              onChange={(e) => handleChange(e)}
-            />
-          </div>
+  return (
+    <div className="home mt-5">
+      <div className="row">
+        <div className="col-12">
+          <h2 className="mb-3">Compare Products</h2>
+          <input
+            type="number"
+            placeholder="Search by price"
+            value={priceSearch}
+            onChange={handleChange}
+          />
         </div>
-        <ProductList
-          products={this.state.products}
-          nameSearch={this.state.nameSearch}
-          compare={compare}
-          priceSearch={this.state.priceSearch}
-        />
-        {compareProducts.length >= 1 && (
-          <CompareTable products={compareProducts} />
-        )}
       </div>
-    );
-  }
+      <ProductList
+        products={products}
+        nameSearch={nameSearch}
+        compare={compare}
+        priceSearch={priceSearch}
+      />
+      {compareProducts.length >= 1 && (
+        <CompareTable products={compareProducts} />
+      )}
+    </div>
+  );
 }
 
 export default Home;
